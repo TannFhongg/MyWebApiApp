f9dfbd1997cdf87ff40a961e4ca5ffc4ff1bbf21
TannFhongg <tuanvv.21ce@vku.udn.vn>
2023-10-24 22:46:20 +0700

    Add project files.

---
+++ MyWebApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,107 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.Id == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                Id = Guid.NewGuid(),
+                Name = hangHoaVM.Name,
+                Price = hangHoaVM.Price
+            };
+            hangHoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true,
+                Data = hanghoa
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hangHoaEdit)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.Id == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+
+                if (id != hangHoa.Id.ToString())
+                {
+                    return BadRequest();
+                }
+                // Update
+                hangHoa.Name = hangHoaEdit.Name;
+                hangHoa.Price = hangHoaEdit.Price;
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                // LINQ [Object] Query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.Id == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                // Update
+                hangHoas.Remove(hangHoa);
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ MyWebApiApp/Models/HangHoa.cs
@@ -0,0 +1,17 @@
+﻿namespace MyWebApiApp.Models
+{
+    public class HangHoaVM
+    {
+        
+        public string Name { get; set; }
+        public double Price { get; set; } = 0;
+
+    }
+    public class HangHoa : HangHoaVM
+    {
+        public Guid Id { get; set; }
+        
+        
+
+    }
+}
